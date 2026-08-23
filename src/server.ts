f5e128f3fe232b578e6243918ebb79a4cc7fc17a
Saad <saadtariq57000@gmail.com>
2026-08-24 02:11:02 +0500

    Testing CI/CD

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3051;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
